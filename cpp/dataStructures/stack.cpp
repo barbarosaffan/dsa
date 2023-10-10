5affe5c6973892d0ed7c79ca9449522f04754d17
barbaros a. yıldırım <barbarosayildirim@gmail.com>
2023-10-10 22:52:24 +0300

    feat: add cpp files

---
+++ cpp/dataStructures/stack.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Stack
+{
+private:
+    vector<int> stack;
+
+public:
+    void push(int);
+    void pop();
+    int top();
+    bool empty();
+};
+
+void Stack::push(int x)
+{
+    stack.push_back(x);
+}
+
+void Stack::pop()
+{
+    stack.pop_back();
+}
+
+int Stack::top()
+{
+    return stack.back();
+}
+
+bool Stack::empty()
+{
+    return stack.empty();
+}
+
+int main()
+{
+    Stack s;
+    s.push(333);
+    s.push(61);
+    s.push(1);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.empty() << endl;
+    return 0;
+}
