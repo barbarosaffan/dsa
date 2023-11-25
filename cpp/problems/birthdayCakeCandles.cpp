bac6c581e5d47d72bf580f11ee87bc37e47dbd37
barbaros a. yıldırım <barbarosayildirim@gmail.com>
2023-11-25 19:16:57 +0300

    feat(cpp): add birthday cake candles

---
+++ cpp/problems/birthdayCakeCandles.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namepsace std;
+
+int birthdayCakeCandles(vector<int> candles)
+{
+    int max = 0;
+    int count = 0;
+
+    for (int i = 0; i < candles.size(); i++)
+    {
+        if (candles[i] > max)
+        {
+            max = candles[i];
+            count = 1;
+        }
+        else if (candles[i] == max)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
