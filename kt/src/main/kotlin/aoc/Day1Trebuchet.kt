8e37e391bb622ed2f7a076e15561667b847bdc72
barbaros a. yıldırım <barbarosayildirim@gmail.com>
2023-12-04 13:50:02 +0300

    feat(kt): add proj structure & aoc day1

---
+++ kt/src/main/kotlin/aoc/Day1Trebuchet.kt
@@ -0,0 +1,48 @@
+package aoc
+
+fun main() {
+    println("Advent of Code Day 1: Trebuchet")
+
+    val resourceName = "aoc/Day1.txt" // Replace with the actual file name
+    val inputStream = Thread.currentThread().contextClassLoader.getResourceAsStream(resourceName)
+    val lines = mutableListOf<String>()
+
+    if (inputStream != null) {
+        inputStream.bufferedReader().useLines {
+            lines.addAll(it.toList())
+        }
+    } else {
+        println("File not found in resources.")
+    }
+
+    var total = 0
+
+    for (line in lines) {
+        val chars = line.toCharArray()
+        var foundFirstDigit = false
+        var firstNum = -1
+        var secondNum = -1
+
+        for (char in chars) {
+            if (char.isDigit()) {
+                if (!foundFirstDigit) {
+                    firstNum = char.toString().toInt()
+                    foundFirstDigit = true
+                } else {
+                    secondNum = char.toString().toInt()
+                }
+            }
+        }
+
+        if (firstNum == -1) return
+
+        total += if (secondNum == -1) {
+            "$firstNum$firstNum".toInt()
+        } else {
+            "$firstNum$secondNum".toInt()
+        }
+
+    }
+
+    println("Total: $total")
+}
