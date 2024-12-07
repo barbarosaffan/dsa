f452383e5657d3dd8d5a236ceb2ef382b76c61e6
barbaros yildirim <barbarosayildirim@gmail.com>
2024-12-07 16:20:16 +0300

    feat(java): add aoc day2 p2

--- java/src/aoc24/days/Day02.java
+++ java/src/aoc24/days/Day02.java
@@ -38,6 +38,44 @@ public class Day02 {
     }
 
     public int solvePart2(List<String> input) {
-        return 0;
+        int totalSafe = 0;
+
+        for (String s : input) {
+            List<String> row = List.of(s.split("\\s+"));
+
+            for (int i = 0; i < row.size(); i++) {
+                boolean allSafe = true;
+                boolean isIncreasing = true;
+                boolean isDecreasing = true;
+
+                for (int j = 1; j < row.size(); j++) {
+                    if (j == i) continue;
+
+                    int previousIndex = (j - 1 == i) ? j - 2 : j - 1;
+                    if (previousIndex < 0) continue;
+
+                    int previous = Integer.parseInt(row.get(previousIndex));
+                    int current = Integer.parseInt(row.get(j));
+
+                    if (current > previous) {
+                        isDecreasing = false;
+                    } else if (current < previous) {
+                        isIncreasing = false;
+                    }
+
+                    if (Math.abs(current - previous) < 1 || Math.abs(current - previous) > 3) {
+                        allSafe = false;
+                        break;
+                    }
+                }
+
+                if (allSafe && (isIncreasing || isDecreasing)) {
+                    totalSafe++;
+                    break;
+                }
+            }
+        }
+
+        return totalSafe;
     }
 }
